775f00a4d97b7741a5ab8c025622752c26d5cd16
lihelin456 <470364000@qq.com>
2016-09-18 16:30:57 +0800

    修改测试

--- HL.Web/Controllers/HomeController.cs
+++ HL.Web/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace HL.Web.Controllers
     {
         public ActionResult Index()
         {
-            
+            return Content("index");
         }
 
         public ActionResult About()
